2e46a265553d19bdb52d1a04ee45e997c2df7111
M.A.Ahmed <ahmedanwer0094@gmail.com>
2024-10-23 10:26:07 +0530

    Update npm packages

--- src/components/__test__/Counter.test.js
+++ src/components/__test__/Counter.test.js
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import Counter from "../Counter";
 import userEvent from "@testing-library/user-event";
 
@@ -12,23 +12,26 @@ describe("Counter component", () => {
   test("check increment", () => {
     render(<Counter initialCount={0} />);
     const incrementButton = screen.getByText("Increment");
-    const countValue1 = screen.getByTestId("count-value");
-    expect(countValue1).toHaveTextContent("0");
-    fireEvent.click(incrementButton);
-    expect(screen.getByTestId("count-value")).toHaveTextContent("1");
+    const countValue = screen.getByTestId("count-value");
+    expect(countValue).toHaveTextContent("0");
+
+    userEvent.click(incrementButton);
+    expect(countValue).toHaveTextContent("1");
   });
 
   test("check decrement", () => {
     render(<Counter initialCount={10} />);
     const decrementButton = screen.getByText("Decrement");
-    fireEvent.click(decrementButton);
+
+    userEvent.click(decrementButton);
     expect(screen.getByTestId("count-value")).toHaveTextContent("9");
   });
 
   test("check reset", () => {
     render(<Counter initialCount={10} />);
     const resetButton = screen.getByText("Reset");
-    fireEvent.click(resetButton);
+
+    userEvent.click(resetButton);
     expect(screen.getByTestId("count-value")).toHaveTextContent("0");
   });
 });
