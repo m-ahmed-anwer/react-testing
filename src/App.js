9cd131aab033cd82653833e72bf352eeb2bb4657
M.A.Ahmed <ahmedanwer0094@gmail.com>
2024-10-23 11:04:01 +0530

    Made some small changes on stylings

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Counter initialCount={0} />
+        <p className="read-the-docs">
+          Made in order to learn React Testing and AWS Amplify
+        </p>
+        <p className="read-the-docs">
+          Created by{" "}
+          <a href="https://github.com/m-ahmed-anwer" target="_blank">
+            Ahmed Anwer
+          </a>
+        </p>
       </header>
     </div>
   );
